6d19802b68254475c5cfe75634e6909e2b4d9da9
Moses <nnythm@gmail.com>
2011-09-02 22:25:02 -0700

    fixed wsgi problem hopefully

--- liondine/scripts/test.py
+++ liondine/scripts/test.py
@@ -7,8 +7,7 @@ from server import user_finder
 from server import create_appointment
 from server import select_appointment
 
-connection = Connection()
-db = connection.liondine
+from liondine.secret import MONGO_URL
 
 class TestSequenceFunctions(unittest.TestCase):
 
@@ -16,6 +15,8 @@ class TestSequenceFunctions(unittest.TestCase):
         clear()
 
     def test_afaculty_join(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         faculty_signup("Moses","Nakamura","mnn2104")
         faculty_collection = db.faculty
         self.assertTrue(user_finder("mnn2104",faculty_collection))
@@ -23,6 +24,8 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertFalse(user_finder("mnn2104",faculty_collection))
 
     def test_bstudent_join(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         student_signup("Moses","Nakamura","mnn2104")
         
         student_collection = db.student
@@ -31,30 +34,44 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertFalse(user_finder("mnn2104",student_collection))
 
     def test_cduplicate_faculty(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         faculty_signup("Moses","Nakamura","mnn2104")
         with self.assertRaises(ValueError):
             faculty_signup("Moses","Nakamura","mnn2104")
 
     def test_dduplicate_student(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         student_signup("Moses","Nakamura","mnn2104")
         with self.assertRaises(ValueError):
             student_signup("Moses","Nakamura","mnn2104")
 
     def test_enew_appointment(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         self.assertTrue(create_appointment(1, 1, 1, 1, 1200, 62))
 
     def test_fappointment_failure(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         with self.assertRaises(ValueError):
             create_appointment(1, 1, 1, 1, 1300, 62)
 
     def test_gappointment_selection(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         self.assertTrue(select_appointment(1, 1, 1, "mnn2104", 1200))
 
     def test_happointment_reselection(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         with self.assertRaises(ValueError):
             select_appointment(1, 1, 1, "mnn2104", 1200)
 
     def test_itrivial_appointment(self):
+        connection = Connection(MONGO_URL)
+        db = connection.liondine
         with self.assertRaises(ValueError):
             select_appointment(2, 1, 1, "mnn2104", 1200)
 
--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,4 @@ current_dir = os.path.dirname(__file__)
 path = "/home/dotcloud/code/liondine/secret.py"
 if not os.path.exists(path):
     os.symlink("/home/dotcloud/secret.py", path)
-application = loadapp('config:production.ini', relative_to=current_dir)
+application = loadapp('config:production.ini', relative_to = current_dir)

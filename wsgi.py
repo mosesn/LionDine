1ca7863a582cd324643755bed72501c678e65fed
Moses <nnythm@gmail.com>
2011-09-02 22:15:55 -0700

    moved symlink from scripts to ..

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os, sys
 from paste.deploy import loadapp
 current_dir = os.path.dirname(__file__)
 
-path = "/home/dotcloud/code/liondine/scripts/secret.py"
+path = "/home/dotcloud/code/liondine/secret.py"
 if not os.path.exists(path):
     os.symlink("/home/dotcloud/secret.py", path)
 application = loadapp('config:production.ini', relative_to=current_dir)

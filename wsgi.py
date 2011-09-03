953f83b67908926f9f1bce1b29492471e0485f56
Moses <nnythm@gmail.com>
2011-09-02 22:08:17 -0700

    added in symlink maker, and wsgi.py so it will actually make the fucking wsgi app

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import os, sys
+from paste.deploy import loadapp
+current_dir = os.path.dirname(__file__)
+
+path = "/home/dotcloud/code/showhn/scripts/secret.py"
+if not os.path.exists(path):
+    os.symlink("/home/dotcloud/secret.py", path)
+application = loadapp('config:production.ini', relative_to=current_dir)
